5ac935e6912ad0704cd438dc496e7e24fd7cce45
iwbl <79720513@qq.com>
2019-11-14 17:22:08 +0800

    公历转农历方法完成

--- lunarFun.js
+++ lunarFun.js
@@ -48,12 +48,19 @@ class LunarFunClass {
     }
 
     /**
+     * 必传参数的默认参数，如果必传参数没有传则会抛出一个错误
+     */
+    _throwIfMissing() {
+        throw new Error('Missing parameter');
+    }
+
+    /**
      * 传入年份和其对应的十六进制字符串，返回JS对象表示的数据
      * 传入的十六进制字符串不包括 “0x” 前缀
      * @param { number } year
      * @param { string } numStr
      */
-    toJSON(year, numStr) {
+    toJSON(year = this._throwIfMissing(), numStr = this._throwIfMissing()) {
         if (!numStr) {
             return '';
         }
@@ -110,7 +117,7 @@ class LunarFunClass {
      * @param year
      * @returns {boolean}
      */
-    isLeapYear(year) {
+    isLeapYear(year = this._throwIfMissing()) {
         /**
          * 现在的公历是格里高利历，是公元1582年以后使用的，之前使用的是儒略历。
          * 格里高利历闰年的定义：世纪年中能被400整除的，和非世纪年中能被4整除的年份(即能被400整除的，或者不能被100整除而能被4整除的年份)
@@ -143,7 +150,7 @@ class LunarFunClass {
      * @param year
      * @returns {string}
      */
-    getHeavenlyStems(year) {
+    getHeavenlyStems(year = this._throwIfMissing()) {
         /**
          * 公元年数先减 3，除以 10 余数所对应的的天干是数组 HeavenlyStems 里的第几个项。也就是余数 -1 作为数组下标得出的值
          * 以 2010(庚寅) 年为例，年份减 3 得基数 2007 ,除以 10 得余数 7, 第 7 个是 '庚'。 也就是 HeavenlyStems[7-1] 为 '庚'
@@ -169,7 +176,7 @@ class LunarFunClass {
      * @param year
      * @returns {string}
      */
-    getEarthlyBranches(year) {
+    getEarthlyBranches(year = this._throwIfMissing()) {
         /**
          * 公元年数先减 3，除以 12 余数所对应的的地支是数组 EarthlyBranches 里的第几个项。也就是余数 -1 作为数组下标得出的值
          * 以 2010(庚寅) 年为例，年份减 3 得基数 2007 ,除以 12 得余数 3, 第 3 个是 '寅'。 也就是 EarthlyBranches[3-1] 为 '庚'
@@ -195,7 +202,7 @@ class LunarFunClass {
      * @param year
      * @returns {string}
      */
-    getZodiac(year) {
+    getZodiac(year = this._throwIfMissing()) {
         /**
          * 公元年数先减 3，除以 12 余数所对应的的生肖是数组 Zodiac 里的第几个项。也就是余数 -1 作为数组下标得出的值
          * 以 2010(虎) 年为例，年份减 3 得基数 2007 ,除以 12 得余数 3, 第 3 个是 '虎'。 也就是 Zodiac[3-1] 为 '虎'
@@ -222,7 +229,7 @@ class LunarFunClass {
      * @param month
      * @returns {number}
      */
-    getMonthNumberDays(year, month) {
+    getMonthNumberDays(year = this._throwIfMissing(), month = this._throwIfMissing()) {
         let FebDays = this.isLeapYear(year) ? 29 : 28;
         let monthNumberDaysArr = ['', 31, FebDays, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         return monthNumberDaysArr[month];
@@ -237,7 +244,7 @@ class LunarFunClass {
      * @param run
      * @returns {number}
      */
-    getLunarMonthNumberDays(year, month, isRun = false) {
+    getLunarMonthNumberDays(year = this._throwIfMissing(), month = this._throwIfMissing(), isRun = false) {
         let yearData = this.LUNAR_INFO.YEAR_INFO[year - this.LUNAR_INFO.MIN_YEAR];
         let yearDataInfo = this.toJSON(+year, yearData);
 
@@ -254,7 +261,7 @@ class LunarFunClass {
      * 如果传入的参数不规范，则返回 0
      * @param year
      */
-    getLunarYearDaysTotal(year) {
+    getLunarYearDaysTotal(year = this._throwIfMissing()) {
         if (!year) {
             console.warn('输入的年份参数有误');
             return 0;
@@ -273,15 +280,191 @@ class LunarFunClass {
         }
     }
 
+    /**
+     * 输入农历日期，输出日期距离那年正月初一的天数
+     * 第4个参数是输入的月份是否为闰月，默认为 false
+     * @param year
+     * @param month
+     * @param day
+     * @param run
+     * @returns {number}
+     */
+    distanceLunarFirstDays(year = this._throwIfMissing(), month = this._throwIfMissing(), day = this._throwIfMissing(), isRun = false) {
+        let yearData = this.LUNAR_INFO.YEAR_INFO[year - this.LUNAR_INFO.MIN_YEAR];
+        let yearDataInfo = this.toJSON(+year, yearData);
 
+        if (!yearDataInfo.isRun) { // 如果不是闰年 返回1月到输入月份前一个月的天数总和加上 日子-1
+            return yearDataInfo.monthsDays.slice(0, month-1).reduce((arr, cur) => {
+                return arr + cur;
+            }) + day - 1;
+        } else { // 如果是闰年，存在多种情况
+            // 如果输入的月份比闰月小，或者输入的月份值等于闰月但isRun为false，则和不是闰年一样输出
+            // month < yearDataInfo.runMonth 的话，isRun一定是false，不是则说明输入的值有误
+            if (month <= yearDataInfo.runMonth && !isRun) {
+                return yearDataInfo.monthsDays.slice(0, month-1).reduce((arr, cur) => {
+                    return arr + cur;
+                }) + day - 1;
+            }
+            // 输入的月份和闰月一样，且isRun为true，返回1月到输入月份的的天数和加上日子-1
+            if (month === yearDataInfo.runMonth && isRun) {
+                return yearDataInfo.monthsDays.slice(0, month).reduce((arr, cur) => {
+                    return arr + cur;
+                }) + day - 1;
+            }
+            // 输入的月份比闰月大，输出的月份总和要加上闰月
+            if (month > yearDataInfo.runMonth) {
+                return yearDataInfo.monthsDays.slice(0, month-1).reduce((arr, cur) => {
+                    return arr + cur;
+                }, yearDataInfo.runMonthDays) + day - 1;
+            }
 
+        }
+    }
 
-    demo() {
-        console.log(this.LUNAR_INFO.CHINESE_MONTH);
+    /**
+     * 输入两个公历日期对象，输出两个日期间隔的天数
+     * @param date1
+     * @param date2
+     * @returns {number}
+     */
+    distanceDate(date1 = this._throwIfMissing(), date2 = this._throwIfMissing()) {
+        // 这里注意如果是手动 new Date(y, m, d); 来声明日期对象，月份要 -1，因为js日期对象月份从0开始 0-11
+        let distance = date1 - date2; // 以毫秒计的运行时长
+        return Math.floor(Math.abs(distance) / 1000 / 60 / 60 / 24); // 相差的毫秒数转为天数
+    }
+
+    /**
+     * 输入公历年月日，返回其日期对象
+     * 因为月份在日期对象里面需要 -1(js日期对象月份从0开始 0-11)，时常忘记，所以这里写一个方法
+     * @param year
+     * @param month
+     * @param day
+     * @returns {Date}
+     */
+    getDateYMD(year = this._throwIfMissing(), month = this._throwIfMissing(), day = this._throwIfMissing()) {
+        return new Date(+year, month - 1, +day);
+    }
+
+    /**
+     * 传入公历年月日返回农历年月日数组
+     * 数组的第四个项是在年份为闰年的时候决定输出的月份是否是闰月
+     */
+    /**
+     * 
+     * @param { number } year 
+     * @param { number } month 
+     * @param { number } day 
+     */
+    gregorianToLunal(year = this._throwIfMissing(), month = this._throwIfMissing(), day = this._throwIfMissing()) {
+        let yearData = this.LUNAR_INFO.YEAR_INFO[year - this.LUNAR_INFO.MIN_YEAR];
+        let yearDataInfo = this.toJSON(+year, yearData);
+
+        /**
+         * 以输入年份的农历正月初一对应的月份和天数来作为基准
+         * 如果输入的月份和天数比基准 大，说明输出的农历年份是同年
+         * 如果输入的月份和天数比基准 小，说明输出的农历年份要比同年公历小一年
+         */
+        let compare = 0; // 0 表示输入的月份天数和基准一致, 即正月初一 || 1 表示输入的月份天数比基准 大 || -1 表示输入的月份天数比基准 小
+        if (month > yearDataInfo.firstMonth) { // 输入的月份和比正月初一对应的月份大，
+            compare = 1;
+        } else if (month < yearDataInfo.firstMonth) { // 输入的月份比正月初一对应的月份小
+            compare = -1;
+        } else if (+month === yearDataInfo.firstMonth) { // 输入的月份和正月初一的月份相同，这个时候比较天数
+            if (day > yearDataInfo.firstDay) {
+                compare = 1;
+            } else if (day < yearDataInfo.firstDay) {
+                compare = -1;
+            } else if (+day === yearDataInfo.firstDay) {
+                compare = 0;
+            }
+        }
+
+        let lunalYear = 0; // 输出的农历年份
+        let lunalMonth = 0; // 输出的农历月份
+        let lunalDay = 0; // 输出的农历天
+        let lunalIsRun = false; // 如果输出的农历年份是闰年，此参数有效，判断输出的月份是否是闰月， 默认false
+
+        if (compare === 1) { // 输入的月份天数比基准 大， 使用同输入年份的农历年份数据
+            lunalYear = +year;
+        } else if (compare === -1) { // 输入的月份天数比基准 小，使用同输入年份 上一年 的农历数据
+            lunalYear = year - 1;
+            yearData = this.LUNAR_INFO.YEAR_INFO[lunalYear - this.LUNAR_INFO.MIN_YEAR];
+            yearDataInfo = this.toJSON(lunalYear, yearData);
+        } else if (compare === 0) { // 输入的月份天数和基准一致
+            lunalYear = +year;
+            lunalMonth = 1;
+            lunalDay = 1;
+        }
+
+        let differDays = this.distanceDate(this.getDateYMD(lunalYear, yearDataInfo.firstMonth, yearDataInfo.firstDay), this.getDateYMD(year, month, day)); // 输入的公历年月日和其所在农历正月初一相差的天数
+        let monthsTotalArr = [...yearDataInfo.monthsDays]; // 农历所有月份组成的数组，包括闰月，
+        if (yearDataInfo.isRun) { // 如果有闰月，则在原来的月份后面插入闰月
+            monthsTotalArr.splice(yearDataInfo.runMonth, 0, yearDataInfo.runMonthDays);
+        }
+        let monthsTotalArrIndex = 0; // 农历月份处在monthsTotalArr的下标
+        let reduceBreak = false; // 额外参数，用来判断是否还进行计算 monthsTotalArrIndex 和 lunalDay
+        monthsTotalArr.reduce((accumulator, currentValue, index) => {
+            // 与正月初一相差的天数+1即是正月初一到输入年月日的天数，例如1号到3号相差2天，但1号到3号总共3天
+            if (((accumulator + currentValue) >= (differDays + 1)) && !reduceBreak) {
+                reduceBreak = true;
+                monthsTotalArrIndex = index;
+                // 天数 = 月份的天数 - (农历前几个月的总和 - (公历相差天数 + 1))
+                lunalDay = monthsTotalArr[index] - ((accumulator + currentValue) - (differDays + 1))
+            }
+            return accumulator + currentValue
+        }, 0);
+        
+        /**
+         * monthsTotalArrIndex 是所有农历月份数组的下标，如果不是闰年 monthsTotalArrIndex+1 就表示当前农历月份
+         * 如果是闰年，monthsTotalArrIndex+1 <= 闰月， 则 monthsTotalArrIndex+1 表示当前农历月份
+         * 如果是闰年，monthsTotalArrIndex+1 > 闰月，则 monthsTotalArrIndex 表示当前农历月份
+         * 如果 monthsTotalArrIndex+1 刚好比闰月打一个月，则 lunalIsRun 为 true
+         */
+        if (yearDataInfo.isRun) {
+            if ((monthsTotalArrIndex + 1) <= yearDataInfo.runMonth) {
+                lunalMonth = monthsTotalArrIndex + 1;
+            } else {
+                lunalMonth = monthsTotalArrIndex;
+                // (monthsTotalArrIndex + 1) - yearDataInfo.runMonth === 1 缩写一下就是下面的判断
+                if (monthsTotalArrIndex === yearDataInfo.runMonth) {
+                    lunalIsRun = true
+                }
+            }
+        } else {
+            lunalMonth = monthsTotalArrIndex + 1;
+        }
+
+        return [+lunalYear, +lunalMonth, +lunalDay, lunalIsRun];
+    }
+
+    /**
+     * 传入农历返回公历年月日数组，第四个参数在农历年份是闰年的时候决定输入的是正常月份还是闰月
+     * @param year
+     * @param month
+     * @param day
+     * @param isRun
+     */
+    lunalToGregorian(year, month, day, isRun = false) {
+
     }
 };
 LunarFunClass.prototype.LUNAR_INFO = LUNAR_INFO;
 let lunarFun = new LunarFunClass();
 
 
-console.log(lunarFun.getLunarYearDaysTotal(2000))
+console.log(lunarFun.gregorianToLunal(1906, 7, 23))
+
+/**
+ * 以下代码是测试代码，没反应说明代码没问题
+ * 测试代码可删除
+ */
+
+// gregorianToLunal() 方法
+ console.assert(JSON.stringify(lunarFun.gregorianToLunal(1906, 5, 22)) === '[1906,4,29,false]', 'gregorianToLunal()方法出错');
+ console.assert(JSON.stringify(lunarFun.gregorianToLunal(1906, 5, 23)) === '[1906,4,1,true]', 'gregorianToLunal()方法出错');
+ console.assert(JSON.stringify(lunarFun.gregorianToLunal(2000, 2, 4)) === '[1999,12,29,false]', 'gregorianToLunal()方法出错');
+ console.assert(JSON.stringify(lunarFun.gregorianToLunal(2000, 12, 31)) === '[2000,12,6,false]', 'gregorianToLunal()方法出错');
+ console.assert(JSON.stringify(lunarFun.gregorianToLunal(2000, 4, 6)) === '[2000,3,2,false]', 'gregorianToLunal()方法出错');
+
+
+
